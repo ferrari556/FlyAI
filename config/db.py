6543b336c603f2a967960f2f6972d15c9bd016c4
KPH <audwlsdl97@naver.com>
2024-01-25 16:18:40 +0900

    Completion

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine('mysql+pymysql://root:1234@localhost:3306/test')
+engine = create_engine('mysql+pymysql://root:1234@localhost:3306/mysql')
 meta = MetaData()
 conn = engine.connect()
--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def home():
     return {'msg' : 'Main'}
 
 if __name__ == "__main__":
-    uvicorn.run("main.py:app", host = "0.0.0.0", port = 8000, reload = True)
+    uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
 
