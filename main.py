767e46d2dfa19203e5bb4cd8a7c88fd459e4a2ba
KPH <audwlsdl97@naver.com>
2024-01-28 18:56:43 +0900

    Modify Code

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ app.include_router(users_route.router, prefix="", tags=["Users"])
 
 @app.get('/')
 def home():
-    return {'msg' : 'Main'}
+    return {'msg' : 'Creating DB'}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host = "127.0.0.1", port = 8000, reload = True)
